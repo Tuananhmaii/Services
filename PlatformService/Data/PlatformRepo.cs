2dca969369b033b84dec0e0047c93524cd0f9b41
Tuananhmaii <anhmttgcd201452@fpt.edu.vn>
2023-03-21 22:47:01 +0700

    Adding sample data & usage of in memory db

---
+++ PlatformService/Data/PlatformRepo.cs
@@ -0,0 +1,38 @@
+﻿using PlatformService.Model;
+
+namespace PlatformService.Data
+{
+    public class PlatformRepo : IPlatformRepo
+    {
+        private readonly ApplicationDbContext _db;
+
+        public PlatformRepo(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public void CreatePlatform(Platform plat)
+        {
+            if(plat == null)
+            {
+                throw new ArgumentNullException(nameof(plat));
+            }
+
+            _db.Add(plat);
+        }
+
+        public IEnumerable<Platform> GetAllPlatforms()
+        {
+            return _db.Platforms.ToList();
+        }
+
+        public Platform GetPlatformById(int id)
+        {
+            return _db.Platforms.FirstOrDefault(n => n.Id == id);
+        }
+
+        public bool SaveChanges()
+        {
+            return (_db.SaveChanges() >= 0);
+        }
+    }
+}
---
+++ PlatformService/Data/PrepDb.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using PlatformService.Model;
+
+namespace PlatformService.Data
+{
+    public static class PrepDb
+    {
+        public static void PrepPopulation(IApplicationBuilder app)
+        {
+            using (var serviceScope = app.ApplicationServices.CreateScope())
+            {
+                SeedData(serviceScope.ServiceProvider.GetService<ApplicationDbContext>());
+            }
+
+        }
+        private static void SeedData(ApplicationDbContext _db)
+        {
+            if (!_db.Platforms.Any())
+            {
+                Console.WriteLine("Seeding Data...");
+                _db.Platforms.AddRange(
+                    new Platform() { Name="Dot Net", Publisher="Microsoft", Cost="Free"},
+                    new Platform() { Name = "SQL Server", Publisher = "Microsoft", Cost = "Free" },
+                    new Platform() { Name = "Kubernetes", Publisher = "Cloud Native", Cost = "Free" }
+                    );
+                _db.SaveChanges();
+            }
+            else
+            {
+                Console.WriteLine("Already have data");
+            }
+        }
+    }
+}
